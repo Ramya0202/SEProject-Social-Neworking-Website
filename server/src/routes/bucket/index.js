05b8bdab842c763de82bf68c9c8c2a2b0155d455
Ramya0202 <ramyaraj2212@gmail.com>
2023-04-29 04:09:22 -0400

    Final server changes

--- server/src/routes/bucket/index.js
+++ server/src/routes/bucket/index.js
@@ -24,12 +24,9 @@ router.put("/:filename", upload.single("file"), (req, res) => {
   try {
     const { filename } = req.params;
     const filePath = `src/uploads/images/${filename}`;
-    // Check if the file exists
     if (fs.existsSync(filePath)) {
-      // Remove the existing file
       fs.unlinkSync(filePath);
     }
-    // Do something with the new file here
     return res.status(200).json("File updated successfully");
   } catch (error) {
     console.error(error);
