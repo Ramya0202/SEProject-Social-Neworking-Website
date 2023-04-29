68c15fe5182167712b4c7eb4625d9199785fc1e6
bgangarapu@albany.edu <bgangarapu@albany.edu>
2023-04-29 19:52:34 -0400

    comment

--- client/src/redux/action/FollowersAction.jsx
+++ client/src/redux/action/FollowersAction.jsx
@@ -1,7 +1,7 @@
 import * as UserAPi from "../../services/User";
 
 export const listAllFollowers = (id) => async (dispatch) => {
-  dispatch({ type: "LIST_ALL_FOLLOWERS_START" });
+  dispatch({ type: "LIST_ALL_FOLLOWERS_START" }); //List of followers
   try {
     const data = await UserAPi.getAllFollowerById(id);
     console.log({ data });
