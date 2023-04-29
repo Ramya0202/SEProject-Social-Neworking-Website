d09cf87a027a0ba0919b89cabeed42042596b7bc
Chakri Penugonda <chakripenugonda9@gmail.com>
2023-04-29 03:40:42 -0400

    announcements

---
+++ server/src/announcement/index.js
@@ -0,0 +1,36 @@
+import AnnouncementModel from "../../model/announcement/index.js";
+import { createNotification } from "../notification/index.js";
+
+export const createAnnouncement = async (req, res) => {
+  const { announcement, users, senderId } = req.body;
+
+  try {
+    const response = await AnnouncementModel.create({
+      announcement,
+      users,
+    });
+    // Send notification to all users using for...of loop
+    for (const userId of users) {
+      await createNotification(userId, announcement, "", senderId);
+    }
+    res.status(201).json({
+      success: true,
+      data: response,
+    });
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+export const getAllAnnouncements = async (req, res) => {
+  try {
+    const announcements = await AnnouncementModel.find().populate("users");
+
+    res.status(200).json({
+      success: true,
+      data: announcements,
+    });
+  } catch (error) {
+    console.log({ error });
+  }
+};
